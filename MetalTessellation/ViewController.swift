5490a0cc71a0c1aa91d1348fb79bee4ab865b680
Makoto Ikeda <mike.app.info@gmail.com>
2017-01-28 19:50:51 +0900

    Initial Commit

---
+++ MetalTessellation/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MetalTessellation
+//
+//  Created by M.Ike on 2017/01/28.
+//  Copyright © 2017年 M.Ike. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
